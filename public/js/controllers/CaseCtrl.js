b70500f5218bcfea7be2f2010fa9a5489c56740f
Adam <adam@petnet.io>
2017-02-06 15:16:32 -0800

    input fields

--- public/js/controllers/CaseCtrl.js
+++ public/js/controllers/CaseCtrl.js
@@ -4,16 +4,20 @@ angular.module('CaseCtrl', []).controller('CaseController', ['Case', '$scope', f
   $scope.tagline = 'The square root of life is pi!'; 
   console.log($scope.tagline);
   $scope.create = create;
+
   $scope.getCases = getCases;
+  $scope.caseName;
+  $scope.caseUrl;
 
   function create() {
     var data = {
-      name: 'Broken Arm',
-      image_url: 'www.google.com',
+      name: $scope.caseName,
+      image_url: $scope.caseUrl,
     }
     Case.create(data)
       .then(function(newCase) {
         console.log(newCase);
+        getCases();
       });
   } 
 
